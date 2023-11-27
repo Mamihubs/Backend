090c23ff19f75d1e855ab74e45e17abca5fe2213
pysavantcodes <pysavego@gmail.com>
2023-11-27 19:59:42 +0100

    fixed product creation

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,14 +1,22 @@
-import mongoose, { Document, Schema, Model } from 'mongoose';
+import mongoose, { Document, Schema, Model } from "mongoose";
 
 // Define interfaces for your schema
 interface IVariation extends Document {
   variation: string;
   price: number;
-  state: 'active' | 'suspended' | 'out of stock';
+  state: "active" | "suspended" | "out of stock";
   barcode?: string;
   volume?: string;
   weight?: string;
   quantity: number;
+  color?: string;
+}
+
+interface IShipping extends Document {
+  width: number;
+  height: number;
+  weight: number;
+  length: number;
 }
 
 interface IProduct extends Document {
@@ -20,9 +28,14 @@ interface IProduct extends Document {
   category_id: mongoose.Types.ObjectId;
   variations: IVariation[];
   images: any[];
-  is_flash_sale:boolean;
+  is_flash_sale: boolean;
   // price_surcharge?: number;
-  // price_discount?: number;
+  price_discount?: number;
+  other_info: string;
+  product_sku: string;
+  product_quantity: number;
+  tags: string[];
+  shipping: IShipping[];
   // date_start?: Date;
   // date_end?: Date;
   // available_threshold?: string;
@@ -31,66 +44,85 @@ interface IProduct extends Document {
   // website_description?: string;
 }
 
+const ShippingSchema = new Schema<IShipping>({
+  width: { type: Number },
+  height: { type: Number },
+  weight: { type: Number },
+  length: { type: Number },
+});
+
 // Create VariationSchema
-const VariationSchema = new Schema<IVariation>(
-  {
-    variation: String,
-    price: Number,
-    state: { type: String, default: "active", enum: ["active", "suspended", "out of stock"] },
-    barcode: { type: String },
-    volume: { type: String },
-    weight: { type: String },
-    quantity: {
-      type: Number,
-      default: 0
-    }
-  }
-);
+const VariationSchema = new Schema<IVariation>({
+  variation: String,
+  price: Number,
+  state: {
+    type: String,
+    default: "active",
+    enum: ["active", "suspended", "out of stock"],
+  },
+  barcode: { type: String },
+  volume: { type: String },
+  weight: { type: String },
+  color: { type: String },
+  quantity: {
+    type: Number,
+    default: 0,
+  },
+});
 
 // Create ProductSchema
-const ProductSchema = new Schema<IProduct>({
-  product_name: {
-    type: String,
-    required: true
+const ProductSchema = new Schema<IProduct>(
+  {
+    product_name: {
+      type: String,
+      required: true,
+    },
+    product_desc: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      default: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    created_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    category_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+    variations: [VariationSchema],
+    images: [],
+    is_flash_sale: {
+      type: Boolean,
+      default: false,
+    },
+    // price_surcharge: { type: Number },
+    price_discount: { type: Number },
+    other_info: { type: String },
+    product_sku: { type: String },
+    product_quantity: { type: Number },
+    tags: [],
+    shipping: [ShippingSchema],
+    // date_start: { type: Date },
+    // date_end: { type: Date },
+    // available_threshold: { type: String },
+    // website_meta_title: { type: String },
+    // website_meta_keywords: { type: String },
+    // website_description: { type: String },
   },
-  product_desc: {
-    type: String,
-    required: true
-  },
-  status: {
-    type: Boolean,
-    default: true
-  },
-  brand: {
-    type: String,
-    required: true
-  },
-  created_by: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  category_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Category'
-  },
-  variations: [VariationSchema],
-  images: [],
-  is_flash_sale:{
-    type:Boolean,
-    default:false
-  }
-  // price_surcharge: { type: Number },
-  // price_discount: { type: Number },
-  // date_start: { type: Date },
-  // date_end: { type: Date },
-  // available_threshold: { type: String },
-  // website_meta_title: { type: String },
-  // website_meta_keywords: { type: String },
-  // website_description: { type: String },
-},
-{ timestamps: true }
+  { timestamps: true }
 );
 
 // Export the model with its TypeScript type
-const ProductModel: Model<IProduct> = mongoose.model<IProduct>('Product', ProductSchema);
+const ProductModel: Model<IProduct> = mongoose.model<IProduct>(
+  "Product",
+  ProductSchema
+);
 export default ProductModel;
--- src/validations/productValidations.ts
+++ src/validations/productValidations.ts
@@ -8,7 +8,15 @@ export const ProductValidation = (data: object) => {
     barcode: Joi.string().optional(),
     volume: Joi.string().optional(),
     weight: Joi.string().optional(),
-    quantity: Joi.number().default(0)
+    color: Joi.string().optional(),
+    quantity: Joi.number().default(0),
+  });
+
+  const shippingSchema = Joi.object({
+    weight: Joi.number().required(),
+    height: Joi.number().required(),
+    width: Joi.number().required(),
+    length: Joi.number().required(),
   });
 
   const productSchema = Joi.object({
@@ -16,16 +24,21 @@ export const ProductValidation = (data: object) => {
     product_desc: Joi.string().required(),
     status: Joi.boolean().default(true),
     brand: Joi.string().required(),
-    created_by: Joi.string().required(),  // assuming it will be converted to ObjectId later
-    category_id: Joi.string().required(),    // assuming it will be converted to ObjectId later
+    created_by: Joi.string().required(), // assuming it will be converted to ObjectId later
+    category_id: Joi.string().required(), // assuming it will be converted to ObjectId later
     variations: Joi.array().items(variationSchema),
-    images: Joi.array().items(Joi.string())  // assuming images are stored as URLs or identifiers
+    images: Joi.array().items(Joi.string()),
+    price_discount: Joi.number().required(),
+    other_info: Joi.string().required(),
+    product_sku: Joi.string().required(),
+    product_quantity: Joi.number().required(),
+    tags: Joi.array().items(Joi.string()).required(),
+    shipping: Joi.array().items(shippingSchema).required(),
     // You can add more fields here based on your commented-out fields if they are required
   });
 
   return productSchema.validate(data);
 };
-
 
 export const UpdateProductValidation = (data: object) => {
   const variationSchema = Joi.object({
@@ -35,16 +48,29 @@ export const UpdateProductValidation = (data: object) => {
     barcode: Joi.string().optional(),
     volume: Joi.string().optional(),
     weight: Joi.string().optional(),
+    color: Joi.string().optional(),
+  });
+  const shippingSchema = Joi.object({
+    weight: Joi.number().required(),
+    height: Joi.number().required(),
+    width: Joi.number().required(),
+    length: Joi.number().required(),
   });
   const productSchema = Joi.object({
     product_name: Joi.string().optional(),
     product_desc: Joi.string().optional(),
     status: Joi.boolean().optional().default(true),
     brand: Joi.string().optional(),
-    created_by: Joi.string().optional(),  // assuming it will be converted to ObjectId later
+    created_by: Joi.string().optional(), // assuming it will be converted to ObjectId later
     // categ_id: Joi.string().optional(),  // assuming it will be converted to ObjectId later
     variations: Joi.array().items(variationSchema).optional(),
-    images: Joi.array().items(Joi.string()).optional()  // assuming images are stored as URLs or identifiers
+    images: Joi.array().items(Joi.string()).optional(), // assuming images are stored as URLs or identifiers
+    price_discount: Joi.number().required(),
+    other_info: Joi.string().required(),
+    product_sku: Joi.string().required(),
+    product_quantity: Joi.number().required(),
+    tags: Joi.array().items(Joi.string()),
+    shipping: Joi.items(shippingSchema).required(),
     // You can add more fields here based on your commented-out fields if they are required
   });
 
