cc1e95af1de5b1dc3fd8eced72063f53eb222151
baniex <banjibaniex1@gmail.com>
2024-02-05 23:52:23 +0100

    making progress

--- src/controllers/courierController.ts
+++ src/controllers/courierController.ts
@@ -15,6 +15,70 @@ export class CourierController {
     MerchantID: "11200001",
   };
 
+
+
+
+
+
+
+  getCourierTokenFunction = async (requestType) => {
+
+    let client;
+
+
+  if(requestType === "/get-delivery-town"){
+
+
+    if (process.env.MONGO_DB_CONNECTION_STRING) {
+      client = new MongoClient(process.env.MONGO_DB_CONNECTION_STRING);
+      await client.connect();
+    }
+
+     const checkForAvailableToken = await client
+       ?.db("mamihub")
+       .collection("courier_token")
+       .findOne({ courierName: "courierName" });
+     console.log(checkForAvailableToken);
+     if (checkForAvailableToken) {
+      const headers = {
+        Authorization: `Bearer ${checkForAvailableToken.courierToken}`,
+        "Content-Type": "application/json",
+      };
+
+      let response = axios
+        .get("http://api.courierplus-ng.com/api/v1/GetOriginDestination", {
+          headers,
+        })
+        .then((response) => {
+          console.log("Response:", response.data);
+        })
+        .catch((error) => {
+          console.error(
+            "Error:",
+            error.response ? error.response.data : error.message
+          );
+        });
+    }
+
+
+
+
+
+  }
+
+    }
+    if(requestType === "/get-destinations"){
+
+    }
+
+
+
+
+
+
+
+    
+
   getCourierToken = async (req: Request, res: Response) => {
     let client;
 
@@ -137,6 +201,17 @@ export class CourierController {
       res.send("I see you my bro");
     }
   };
+
+  getDeliveryTown = async (req: Request, res: Response) => {
+    console.log(req.body);
+    let {originStation, originStationCode} = req.body;
+    console.log(originStation)
+    res.send("chai");
+
+
+
+
+  }
 }
 
 
--- src/routes/courierRoutes.ts
+++ src/routes/courierRoutes.ts
@@ -13,6 +13,7 @@ import CourierController from "../controllers/courierController";
 
 
 router.get("/get-destinations", CourierController.getDestinations)
-router.get("/get-courier-token", CourierController.getCourierToken);
+router.get("/get-courier-token", CourierController.getCourierToken)
+router.post("/get-delivery-town", CourierController.getDeliveryTown)
 
 export default router
